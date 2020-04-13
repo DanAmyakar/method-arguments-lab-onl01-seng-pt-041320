4a8413afb85a3c1c64b87ad81e145a7539c10c3c
Daniel Amyakar <dan.rockit@gmail.com>
2020-04-13 18:41:55 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,7 @@
+def introduction(nam)
+  puts "Hi, my name is #{nam}."
+end
+
+def introduction_with_language(nam, lang)
+  puts "Hi, my name is #{nam} and I am learning to program in #{lang}."
+end
